a03784e6608aeba4d638e4ea9e7b750fe3e594f5
Mutiara Callista Pramestya <92364379+mutiaracallistapramestya@users.noreply.github.com>
2021-11-14 06:36:39 -0800

    Add files via upload

---
+++ 2117051013_Mutiara Callista Pramestya_Posttest2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main () {
+
+//Mutiara  Callista Pramestya
+//2117051013	
+//Kelas C
+//Post Test 2
+
+float HargaSebuahPena;
+float HargaSebuahBuku;
+float HargaSebuahPenghapus;
+float HargaSebuahPenggaris;
+float j1 = 13;
+float j2 = 27;
+float j3 = 19;
+float j4 = 38;	
+ 
+cin>>HargaSebuahPena;  
+cin>>HargaSebuahBuku;  
+cin>>HargaSebuahPenghapus; 
+cin>>HargaSebuahPenggaris; 
+
+cout << "                              " << endl;
+
+
+cout<< "                               " << endl;
+cout<< "Harga " <<j1<< " buah Pena      = Rp." << (j1*HargaSebuahPena) << endl;
+cout<< "Harga " <<j2<< " buah Buku      = Rp."  << (j2*HargaSebuahBuku)<< endl;
+cout<< "Harga " <<j3<< " buah Penghapus = Rp." << (j3*HargaSebuahPenghapus) << endl;
+cout<< "Harga " <<j4<< " buah Penggaris = Rp." << (j4*HargaSebuahPenggaris)<< endl;
+
+
+	return 0;
+}
+
+	
+	
+
